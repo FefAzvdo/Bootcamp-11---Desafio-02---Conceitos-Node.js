ec854ffee88c548cd5c5f2333f7c24fb3fda8199
Fernando Augusto <wfernandoaugusto01@gmail.com>
2020-10-07 15:24:17 -0300

    Inittial Commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
 
 // const { v4: uuid } = require('uuid');
 
@@ -11,23 +12,72 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const id = uuid()
+
+  const newRepository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(newRepository)
+
+  return response.json(newRepository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositoryToBeChangedId = repositories.findIndex(repository => repository.id == id)
+
+  if (repositoryToBeChangedId < 0) {
+    return response.status(400).json({ error: "Repository doesn't exist" })
+  }
+
+  repositories[repositoryToBeChangedId].title = title
+  repositories[repositoryToBeChangedId].url = url
+  repositories[repositoryToBeChangedId].techs = techs
+
+  return response.json(repositories[repositoryToBeChangedId])
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoryToBeDeletedId = repositories.findIndex(repository => repository.id == id)
+
+  if (repositoryToBeDeletedId < 0) {
+    return response.status(400).json({ error: "Repository doesn't exist" })
+  }
+
+  repositories.splice(repositoryToBeDeletedId, 1)
+
+  return response.status(204).json({ message: "Repository deleted" })
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoryToByLikedId = repositories.findIndex(repository => repository.id == id)
+
+  if (repositoryToByLikedId < 0) {
+    return response.status(400).json({ message: "Repository doesn't exist" })
+  }
+
+  repositories[repositoryToByLikedId].likes = repositories[repositoryToByLikedId].likes + 1
+
+  return response.json(repositories[repositoryToByLikedId])
+
 });
 
 module.exports = app;
